1e448b90cc20a073495f017fd0eeee9ca9c3a60d
krishnaiitd <kprasad.iitd@gmail.com>
2014-07-02 22:52:37 +0530

    Added config file and My SQL connector

--- class.MySqlConnector.php
+++ class.MySqlConnector.php
@@ -1,6 +1,37 @@
 <?php
-class MySql
+abstract class MySql_Connector
 {
-	function private __construct() {}
+    /**
+     * @var MySQL connector Object
+     */
+    protected static $_instance = NULL;
+
+    /**
+     * Prevent direct object creation
+     */
+    final private function __construct() { }
+
+    /**
+     * Prevent object cloning
+     */
+    final private function __clone() { }
+
+    final public static function getInstance() {
+        if (null !== static::$_instance) {
+            return static::$_instance;
+        }
+        $mySqlUserName = '';
+        $mySqlPassword = '';
+        $mySqlHost  = '';
+        $mySqlPort = '';
+        if (APPLICATION_ENV == 'dev') {
+            $mySqlUserName = defined(LOCAL_MYSQL_USERNAME) ? LOCAL_MYSQL_USERNAME : '';
+            $mySqlPassword = defined(LOCAL_MYSQL_PASSWORD) ? LOCAL_MYSQL_PASSWORD : '';
+            $mySqlHost = defined(LOCAL_MYSQL_HOST) ? LOCAL_MYSQL_HOST : '';
+            $mySqlPort = defined(LOCAL_MYSQL_PORT) ? LOCAL_MYSQL_PORT : '';
+
+        }
+    }
+
 
 }
---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krishna
+ * Date: 02/07/14
+ * Time: 10:35 PM
+ */
+
+/**
+ * Application Environment
+ * dev/live
+ */
+define("APPLICATION_ENV", 'dev');
+
+/**
+ * Local My Sql auth
+ */
+define("LOCAL_MYSQL_USERNAME", 'root');
+define("LOCAL_MYSQL_PASSWORD", '12345');
+define("LOCAL_MYSQL_HOST", 'localhost');
+define("LOCAL_MYSQL_PORT", '');
